a8a40992d6486ff39fa6fff6d0ea659f450f0444
Marcos <marcos.meireles@gmail.com>
2023-05-23 19:48:31 -0300

    atualização de arquivos ATIVIDADE 22

--- Atividade_de_laco_22_05/src/Atividade/ultima_de_laco.java
+++ Atividade_de_laco_22_05/src/Atividade/ultima_de_laco.java
@@ -12,36 +12,33 @@ public class ultima_de_laco {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-
 		System.out.print("Digite o tipo de operação (+, /, *, -): ");
 		String operacao = sc.nextLine();
 		System.out.println("Digite o número que deseja a tabuada");
-		int numero2 = sc.nextInt();
-		
+		float numero2 = sc.nextInt();
 
 		for (int numero = 1; numero <= 10; numero++) {
-			int resultado = 0;
+			float resultado = 0;
 
 			switch (operacao) {
 			case "+":
-				
+
 				resultado = numero + numero2;
-				System.out.println(numero + " + " +numero2+" = " + resultado);
-				
+				System.out.println(numero + " + " + numero2 + " = " + resultado);
+
 				break;
 			case "/":
 				resultado = numero / numero2;
-				if (resultado != 0){
-					System.out.println(numero + " / " +numero2+" = " + resultado);
-				}
+
+				System.out.println(numero + " / " + numero2 + " = " + resultado);
 				break;
 			case "*":
 				resultado = numero * numero2;
-				System.out.println(numero + " x " +numero2+" = " + resultado);
+				System.out.println(numero + " x " + numero2 + " = " + resultado);
 				break;
 			case "-":
 				resultado = numero - numero2;
-				System.out.println(numero + " - " +numero2+" = " + resultado);
+				System.out.println(numero + " - " + numero2 + " = " + resultado);
 				break;
 			default:
 				System.out.println("Operação inválida. Tente novamente.");
