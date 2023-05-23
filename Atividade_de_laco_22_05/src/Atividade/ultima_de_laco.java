b4c3ad055d73cac10ca531fe2807bb8aa56c7231
Marcos <marcos.meireles@gmail.com>
2023-05-23 19:22:12 -0300

    atualização de arquivos ATIVIDADE 23/05

---
+++ Atividade_de_laco_22_05/src/Atividade/ultima_de_laco.java
@@ -0,0 +1,51 @@
+// Faça um programa que receba o tipo de operação a ser realizada 
+//(soma, divisão, multiplicação, subtração) 
+// e imprima a tabuada desse número. ex.: 1x2 = 2, 1x3 = 3
+
+
+package Atividade;
+
+import java.util.Scanner;
+
+public class ultima_de_laco {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+				
+		        System.out.print("Digite o tipo de operação (+, /, *, -): ");
+		        String operacao = sc.nextLine();
+
+		        for (int numero = 1; numero <= 10; numero++) {
+		            int resultado = 0;
+
+		            switch (operacao) {
+		                case "+":
+		                    resultado = numero + 2;
+		                    System.out.println(numero + " + 2 = " + resultado);
+		                    break;
+		                case "/":
+		                    resultado = numero / 2;
+		                    System.out.println(numero + " / 2 = " + resultado);
+		                    break;
+		                case "*":
+		                    resultado = numero * 2;
+		                    System.out.println(numero + " x 2 = " + resultado);
+		                    break;
+		                case "-":
+		                    resultado = numero - 2;
+		                    System.out.println(numero + " - 2 = " + resultado);
+		                    break;
+		                default:
+		                    System.out.println("Operação inválida. Tente novamente.");
+		                    break;
+		            }
+		        }
+
+		        sc.close();
+		    
+		
+
+
+	}
+
+}
