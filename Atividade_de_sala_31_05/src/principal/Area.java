af1eda53b529a5603856378a3d037c3372415955
Marcos <marcos.meireles@gmail.com>
2023-05-31 21:39:53 -0300

    atualização de arquivos ATIVIDADE 31/05

--- Atividade_de_sala_31_05/src/principal/Area.java
+++ Atividade_de_sala_31_05/src/principal/Area.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 import entities.AreaTerreno;
 
-public class Program {
+public class Area {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
