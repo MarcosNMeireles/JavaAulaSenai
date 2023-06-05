809fbf55cc52ae6c3ffdc0ae9f9ca880cb20f1f7
Marcos <marcos.meireles@gmail.com>
2023-06-05 19:30:18 -0300

    atualização de arquivos ATIVIDADE 01/06

--- Atividade_de_sala_01_06/src/principal/Program.java
+++ Atividade_de_sala_01_06/src/principal/Program.java
@@ -1,79 +1,26 @@
 package principal;
 
-import java.util.Scanner;
-
 import entities.Televisor;
 
 public class Program {
 
-	public static void main(String[] args) {
-	/*
-	 * 
-	 * 
-	 * // Criando um objeto da classe Televisor Televisor televisor = new
-	 * Televisor();
-	 * 
-	 * // Ligando o televisor televisor.ligarTelevisor();
-	 * 
-	 * // Aumentando o volume em 1 televisor.aumentarVolume();
-	 * 
-	 * // Subindo o canal em 1 televisor.subirCanal();
-	 * 
-	 * televisor.desligarTelevisor();
-	 * 
-	 * // Exibindo o status atual do televisor televisor.mostrarStatus();
-	 * 
-	 * }
-	 */
+public static void main(String[] args) {
 
-	// Classe Principal
+// Criando um objeto da classe Televisor
+        Televisor televisor = new Televisor();
 
-	Televisor televisor = new Televisor();
-    Scanner sc = new Scanner(System.in);
-    boolean executando = true;
+        // Ligando o televisor
+        televisor.ligarTelevisor();
 
-    while (executando) {
-        System.out.println("\n=== MENU ===");
-        System.out.println("1 - Aumentar volume");
-        System.out.println("2 - Reduzir volume");
-        System.out.println("3 - Subir canal");
-        System.out.println("4 - Descer canal");
-        System.out.println("5 - Ligar televisão");
-        System.out.println("6 - Desligar televisão");
-        System.out.println("7 - Mostrar status da televisão");
-        System.out.println("8 - Sair");
+        // Aumentando o volume em 1
+        televisor.aumentarVolume();
 
-        System.out.print("Escolha uma opção: ");
-        int opcao = sc.nextInt();
+        // Subindo o canal em 1
+        televisor.subirCanal();
 
-        switch (opcao) {
-            case 1:
-                televisor.aumentarVolume();
-                break;
-            case 2:
-                televisor.reduzirVolume();
-                break;
-            case 3:
-                televisor.subirCanal();
-                break;
-            case 4:
-                televisor.descerCanal();
-                break;
-            case 5:
-                televisor.ligarTelevisor();
-                break;
-            case 6:
-                televisor.desligarTelevisor();
-                break;
-            case 7:
-                televisor.mostrarStatus();
-                break;
-            case 8:
-                executando = false;
-                break;
-            default:
-	}
+        // Exibindo o status atual do televisor
+        televisor.mostrarStatus();
+
 }
-    sc.close();
-}
+
 }
