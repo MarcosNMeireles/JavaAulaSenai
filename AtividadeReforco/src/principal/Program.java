9647f69aed7cf87a138117c7e398c0cfa3233a2e
Marcos <marcos.meireles@gmail.com>
2023-06-05 19:50:34 -0300

    atualização de arquivos ATIVIDADE 05/06

--- AtividadeReforco/src/principal/Program.java
+++ AtividadeReforco/src/principal/Program.java
@@ -1,9 +1,28 @@
 package principal;
 
+import java.util.Scanner;
+
+import entities.Funcionario;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+ 
+		Scanner sc = new Scanner (System.in);
+		
+        System.out.print("Digite o nome do funcionário: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Digite o cargo do funcionário: ");
+        String cargo = sc.nextLine();
+
+        System.out.print("Digite o salário do funcionário: ");
+        double salario = sc.nextDouble();
+
+        Funcionario funcionario = new Funcionario(nome, cargo, salario);
+        funcionario.verificarSalarioECargo();
+
+		sc.close();
 
 	}
 
