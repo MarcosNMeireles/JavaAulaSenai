2b77da42f6a292a3434ad24dc87b575fe88747db
Marcos <marcos.meireles@gmail.com>
2023-07-13 21:51:56 -0300

    atualização de arquivos ATIVIDADE BANCO DE DADOS 13/07

---
+++ Front_Noturno/basico_JS/js/basico.js
@@ -0,0 +1,22 @@
+console.log("Hello world, no arquivo js!");
+
+console.log(1+"1");
+console.log(1-"1");
+
+var x=10
+let y=11
+var x = 11
+
+
+
+console.log(typeof(x)) //fala qual o tipo é a variavel
+
+console.log("aula"*2) //tenta multiplicar a string e apresenta um erro
+
+let numero = Number(50) //fala qual o tipo é a variavel
+let numeroString = String(50) 
+let casaDecimais = 10.9988
+
+console.log(typeof(numero)) //fala qual o tipo é a variavel
+console.log(numeroString) //exibe o dado atribuido 
+console.log(typeof(casaDecimais))
