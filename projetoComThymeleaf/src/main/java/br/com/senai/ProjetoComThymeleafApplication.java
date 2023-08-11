f72a7f3475f2d108b4b2e375528c3e57bd01f610
Marcos <marcos.meireles@gmail.com>
2023-08-10 21:44:06 -0300

    atualização de arquivos 10/08

--- projetoComThymeleaf/src/main/java/br/com/senai/ProjetoComThymeleafApplication.java
+++ projetoComThymeleaf/src/main/java/br/com/senai/ProjetoComThymeleafApplication.java
@@ -1,13 +1,21 @@
+// Indica que esse arquivo Java pertence ao pacote (diretório) 'br.com.senai'
 package br.com.senai;
 
+// Importa a classe SpringApplication do Spring Boot, que é usada para iniciar o aplicativo
 import org.springframework.boot.SpringApplication;
+
+// Importa a anotação SpringBootApplication, que configura automaticamente o aplicativo
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Essa anotação marca essa classe como a classe principal do aplicativo
 @SpringBootApplication
 public class ProjetoComThymeleafApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoComThymeleafApplication.class, args);
-	}
+    // Método principal que o computador vai executar ao rodar o programa
+    public static void main(String[] args) {
+        // Aqui usamos a classe SpringApplication para iniciar o aplicativo
+        // Passamos a classe ProjetoComThymeleafApplication como argumento, indicando que é a classe principal do aplicativo
+        SpringApplication.run(ProjetoComThymeleafApplication.class, args);
+    }
 
 }
--- projetoComThymeleaf/src/main/java/br/com/senai/controller/ProdutoController.java
+++ projetoComThymeleaf/src/main/java/br/com/senai/controller/ProdutoController.java
@@ -1,76 +1,88 @@
+// Importações necessárias para as classes e recursos utilizados
 package br.com.senai.controller;
 
 import java.util.List;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired; // Importação necessária para injetar dependências
-import org.springframework.stereotype.Controller; // Indica que esta classe é um controlador Spring
-import org.springframework.ui.Model; // Importação necessária para trabalhar com o Model do Spring
-import org.springframework.validation.BindingResult; // Importação necessária para tratar os resultados de validação
-import org.springframework.validation.Errors; // Importação necessária para trabalhar com a classe Errors
-import org.springframework.web.bind.annotation.GetMapping; // Importação para a anotação de mapeamento GET
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping; // Importação para a anotação de mapeamento POST
+import org.springframework.web.bind.annotation.PostMapping;
 
-import br.com.senai.model.Produto; // Importação da classe de modelo Produto
-import br.com.senai.repository.ProdutoRepository; // Importação do repositório de produtos
+import br.com.senai.model.Produto; // Importa a classe do modelo Produto
+import br.com.senai.repository.ProdutoRepository; // Importa a classe do repositório de Produto
 
+// Anotação que marca esta classe como um controlador
 @Controller
 public class ProdutoController {
-
-    @Autowired 
-    private ProdutoRepository produtoRepository; // Injeção de dependência do repositório de produtos
-
-    @GetMapping("/")    
-    public String paginaPrincipal() {
-        return "index"; // Retorna o nome da View "index" para a página inicial
-    }
-
-    @GetMapping("/produto")
-    public String listarProdutos(Model model) {
-        List<Produto> produto = produtoRepository.findAll(); // Obtém uma lista de produtos do repositório
-        model.addAttribute("produto", produto); // Adiciona a lista de produtos ao Model para uso na View
-        return "produtos"; // Retorna o nome da View "produtos" para exibir a lista de produtos
-    }
-    
-    @GetMapping("/cadastrarProduto")
-    public String paginaAdicionarProduto(Produto produto) {
-    	return "adicionar_produto"; // Retorna o nome da View "adicionar_produto" para a página de cadastro de produtos
-    }
-    
-    @PostMapping("/adicionarProduto")
-    public String adicionaProduto (@Valid Produto produto, Errors erros,
-    								BindingResult result, Model model) {
-    	if(result.hasErrors() || (null != erros && erros.getErrorCount() > 0)) {
-    		return "adicionar_produto"; // Retorna o nome da View "adicionar_produto" se houver erros de validação
-    	}
-    	produtoRepository.save(produto); // Salva o novo produto no repositório
-    	return "redirect:/produto"; // Redireciona para a URL "/produto" após adicionar o produto
-    }
-    
-    @GetMapping("editar/{id}")
-    public String paginaAtualizarProduto(
-    		@PathVariable("id") long id, Model model) {
-    	Produto produto = produtoRepository.findById(id)
-                .orElseThrow(() ->
-    			new IllegalArgumentException("Identificador do produto é inválido"+ id));
-    	model.addAttribute("produto", produto);
-    	return "redirect:editar_produto";
-    }
-    
-    @PostMapping("/atualizar/{id}")
-    public String atualizarProduto(@PathVariable("id") long id,@Valid Produto produto, 
-    		BindingResult result, Model model) {
-    	if (result.hasErrors()) {
-    		produto.setId(id);
-    		return "editar_produto";
-    	}
-    	produtoRepository.save(produto);
-    	return "redirect:/produto";
-    	
-    }
-    		
+	
+	@Autowired
+	private ProdutoRepository produtoRepository; // Injeção de dependência do repositório de Produto
+	
+	// Mapeamento para a página principal
+	@GetMapping("/")
+	public String paginaPrincipal() {
+		return "index"; // Retorna o nome da página a ser exibida (index.html ou similar)
+	}
+	
+	// Mapeamento para listar os produtos
+	@GetMapping("/produto")
+	public String listarProdutos(Model model) {
+		List<Produto> produtos = produtoRepository.findAll(); // Obtém a lista de produtos do repositório
+		model.addAttribute("produtos", produtos); // Adiciona a lista de produtos ao modelo para exibição na página
+		return "produtos"; // Retorna o nome da página de listagem de produtos
+	}
+	
+	// Mapeamento para exibir a página de adicionar produto
+	@GetMapping("/cadastrarProduto")
+	public String paginaAdicionarProduto(Produto produto) {
+		return "adicionar_produto"; // Retorna o nome da página de adicionar produto
+	}
+	
+	// Mapeamento para processar o formulário de adição de produto
+	@PostMapping("/adicionarProduto")
+	public String adicionaProduto(@Valid Produto produto, 
+			Errors erros, BindingResult result, Model model) {
+		if(result.hasErrors() || (null != erros && erros.getErrorCount() > 0)) {
+			return "adicionar_produto"; // Retorna a página de adicionar produto caso haja erros
+		}
+		produtoRepository.save(produto); // Salva o novo produto no repositório
+		return "redirect:/produto"; // Redireciona para a página de listagem de produtos
+	}
+	
+	// Mapeamento para exibir a página de editar produto
+	@GetMapping("/editar/{id}")
+	public String paginaAtualizarProduto(
+			@PathVariable("id") long id, Model model) {
+		Produto produto = produtoRepository.findById(id)
+				.orElseThrow(() -> 
+				new IllegalArgumentException("Identificador do produto é inválido" + id));
+		
+		model.addAttribute("produto", produto); // Adiciona o produto ao modelo para edição
+		return "editar_produto"; // Retorna o nome da página de edição de produto
+	}
+	
+	// Mapeamento para processar o formulário de atualização de produto
+	@PostMapping("/atualizar/{id}")
+	public String atualizarProduto(@PathVariable("id") long id,
+			@Valid Produto produto, BindingResult result,
+			Model model) {
+		if(result.hasErrors()) {
+			produto.setId(id);
+			return "editar_produto"; // Retorna a página de edição de produto caso haja erros
+		}
+		produtoRepository.save(produto); // Atualiza o produto no repositório
+		return "redirect:/produto"; // Redireciona para a página de listagem de produtos
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluirProduto(@PathVariable Long id) {
+	    produtoRepository.deleteById(id);
+	    return "redirect:/produto"; // Redirecionar para a página de listagem após a exclusão
+	}
 }
-
-
